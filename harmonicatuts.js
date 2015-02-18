de84e06d23cb736226b368f1d37e1730c04f8e34
nxpeacock <nguyenxuancong@gmail.com>
2015-02-18 23:53:27 +0700

    Cơ bản play

--- harmonicatuts.js
+++ harmonicatuts.js
@@ -1,8 +1,13 @@
 if (Meteor.isClient) {
+    notDen = 1284, denChamDoi = notDen * 1.5, mocDon = notDen / 2, trangChamDoi = notDen * 2.5, notTrang = notDen * 2;
     Template.karaoke.helpers({
         game: function () {
-            $(document).ready(function(){
-                game = new Phaser.Game(1024, 600, Phaser.AUTO, "game", {preload: preload, create: create, update: update});
+            $(document).ready(function () {
+                game = new Phaser.Game(1024, 600, Phaser.AUTO, "game", {
+                    preload: preload,
+                    create: create,
+                    update: update
+                });
 
                 return game;
             })
@@ -10,7 +15,7 @@ if (Meteor.isClient) {
     });
 
     var preload = function () {
-
+        game.load.spritesheet('play', 'assets/play.png');
     }
 
     var create = function () {
@@ -38,9 +43,10 @@ if (Meteor.isClient) {
          t.fill = "red";
          },waitTime)
          })*/
-        _phachBatDau = game.add.text(50,50,'0',{font : '36px Arial',fill : 'green',align : 'left'})
+        button = game.add.button(50, 10, 'play', actionOnClick, this);
+        _phachBatDau = game.add.text(50, 180, '0', {font: '64px Arial', fill: 'green', align: 'left'});
         var notes = [
-            {id: 0, text: "|",tab : ''},
+            {id: 0, text: "|", tab: ''},
             {id: 1, text: 'Đồ', tab: 4},
             {id: 2, text: 'Rê', tab: -4},
             {id: 3, text: 'Mi', tab: 5},
@@ -51,9 +57,8 @@ if (Meteor.isClient) {
             {id: 8, text: 'Đố', tab: 7}
         ];
 
-        var notDen = 1284, denChamDoi = notDen * 1.5, mocDon = notDen / 2, trangChamDoi = notDen * 2.5, notTrang = notDen * 2;
 
-        var ohSusanaNotes = [
+        ohSusanaNotes = [
             [
                 {id: 1, wait: notDen},
                 {id: 2, wait: notDen},
@@ -120,14 +125,14 @@ if (Meteor.isClient) {
                 {id: 0, wait: trangChamDoi},
             ]
         ];
-        var totalTime = notDen*4;
-        var Y = 100;
+        var totalTime = notDen * 6;
+        var Y = 250;
 
         var noteDisplay = _.template("<%=t%> &nbsp;");
         _ohSusanaWait = [];
         _.each(ohSusanaNotes, function (ohX, i) {
             var step = 10;
-            _.each(ohX,function(oh){
+            _.each(ohX, function (oh) {
                 totalTime += oh.wait;
                 var note = notes[oh.id];
                 var str = note.tab + note.text;
@@ -135,22 +140,33 @@ if (Meteor.isClient) {
                 var t = game.add.text(step, Y, str, {font: '16px Arial', fill: 'white', align: 'left'});
                 _ohSusanaWait.push(
                     {
-                        id : oh.id,
-                        wait : totalTime,
-                        text : t
+                        id: oh.id,
+                        wait: totalTime,
+                        text: t
                     }
                 )
             })
-            Y+=50;
+            Y += 50;
         });
 
-        /*_.each(_ohSusanaWait, function (oh) {
+    }
+
+    var actionOnClick = function () {
+        var totalTime = 0;
+        _(4).times(function (i) {
+            console.log(i)
+            totalTime += i * notDen;
+            _.delay(function () {
+                _phachBatDau.text = ++i;
+            }, totalTime);
+        });
+
+        _.each(_ohSusanaWait, function (oh) {
             Meteor.setTimeout(function () {
                 oh.text.fill = "red";
             }, oh.wait)
-        })*/
+        })
     }
-
     var update = function () {
 
     }
